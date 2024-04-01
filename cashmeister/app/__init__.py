82b6365725410f384af61037d1136b617165568c
Ad Heinz <Heinzad@users.noreply.github.com>
2024-04-01 19:53:03 +1300

    Upgraded app configuration

--- cashmeister/app/__init__.py
+++ cashmeister/app/__init__.py
@@ -1,11 +1,17 @@
 """init
 -- Flask Application Instance 
+
 """ 
 
 from flask import Flask
 
+from config import Config 
+
+#Create the application object as an instance of class Flask 
+#imported from the flask package
 
 app = Flask(__name__)
+app.config.from_object(Config)
 
 
 # avoid circular imports by placing at end of script
