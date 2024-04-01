d39cbd52553e2a8a7def8757dcebeec8b789315f
Ad Heinz <Heinzad@users.noreply.github.com>
2024-04-01 19:55:05 +1300

    Added login view function to routes

--- cashmeister/app/routes.py
+++ cashmeister/app/routes.py
@@ -2,14 +2,17 @@
 -- Home page route
 """
 
-from flask import render_template
+from flask import render_template, flash, redirect, url_for
 from app import app 
+from app.forms import LoginForm
 
 # decorator modifying the function that follows it 
 # used to register functions as callbacks for particular events
 # creating an association between url given as an argument and the function 
 # when a web browser requests any of these urls, flask will invoke the function 
 # and pass its value back to the browser as a response.
+
+# index view function 
 
 @app.route('/')
 @app.route('/index')
@@ -37,3 +40,12 @@ def index():
     ]
     return render_template('index.html', title='Home', user=user, directories=directories)
 
+# login view function 
+
+@app.route('/login', methods=['GET', 'POST'])
+def login(): 
+    form = LoginForm()
+    if form.validate_on_submit(): 
+        flash(f"Login requested for user {form.username.data}, remember_me={form.remember_me.data}")
+        return redirect(url_for('index'))
+    return render_template('login.html', title='Sign In', form=form) 
