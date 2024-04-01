05d914fc3349d016d7554d2e8f7d0a4892737304
Ad Heinz <Heinzad@users.noreply.github.com>
2024-04-01 20:08:22 +1300

    Added flask-sqlalchemy and flask-migrate to application initialisation

--- cashmeister/app/__init__.py
+++ cashmeister/app/__init__.py
@@ -4,6 +4,8 @@
 """ 
 
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate 
 
 from config import Config 
 
@@ -12,7 +14,9 @@ from config import Config
 
 app = Flask(__name__)
 app.config.from_object(Config)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 
 # avoid circular imports by placing at end of script
-from app import routes
+from app import routes, models
